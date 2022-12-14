82f0d0394d779978dae332c92a0f4f3987aa02be
julia <juliabresolin2@gmail.com>
2022-12-13 22:04:08 -0300

    feat: create notification repository

---
+++ src/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,24 @@
+import { Notification } from '../entities/notification';
+import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const notificationRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
+
+describe('Send notification', () => {
+  it('should be able to send a notifications notification', async () => {
+    const sendNotification = new SendNotification(notificationRepository);
+
+    await sendNotification.execute({
+      content: 'This is a notification',
+      category: 'social',
+      recipientId: 'example-recipient-id',
+    });
+
+    expect(notifications).toHaveLength(1);
+  });
+});
