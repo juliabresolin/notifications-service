a608e05300aaffda39184a5fcb558549ce321311
julia <juliabresolin2@gmail.com>
2022-12-14 22:01:45 -0300

    chore: create ts paths

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { NotificationController } from './controllers/notification.controller';
-import { SendNotification } from 'src/application/use-cases/send-notification';
+import { SendNotification } from '@application/use-cases/send-notification';
 import { DatabaseModule } from '../database/database.module';
 
 @Module({
