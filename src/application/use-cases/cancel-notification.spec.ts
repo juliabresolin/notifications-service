659dd7e157e90eca48bac35d48048d5d351e2057
julia <juliabresolin2@gmail.com>
2022-12-14 23:24:36 -0300

    feat: add testing from cancel not exist notification

--- src/application/use-cases/cancel-notification.spec.ts
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -2,11 +2,11 @@ import { Content } from '@application/entities/content';
 import { Notification } from '@application/entities/notification';
 import { InMemoryNotificationRepository } from '@test/repositories/in-memory-notifications-repository';
 import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
 
 describe('Cancel notification', () => {
-  it('should be able to send a notifications notification', async () => {
+  it('should be able to cancel a notifications notification', async () => {
     const notificationRepository = new InMemoryNotificationRepository();
-
     const cancelNotification = new CancelNotification(notificationRepository);
 
     const notification = new Notification({
@@ -26,5 +26,16 @@ describe('Cancel notification', () => {
     expect(notificationRepository.notifications[0].canceledAt).toEqual(
       expect.any(Date),
     );
+  });
+
+  it('should not be able to cancel a non existing notification', async () => {
+    const notificationRepository = new InMemoryNotificationRepository();
+    const cancelNotification = new CancelNotification(notificationRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
   });
 });
